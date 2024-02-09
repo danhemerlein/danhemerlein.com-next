b53bc0adee619cc18aaa5216a2e9440bd86e59ad
Dan Hemerlein <danhemerlein@gmail.com>
2024-02-08 23:36:27 -0500

    add image grid to moodboard

--- app/moodboard/page.tsx
+++ app/moodboard/page.tsx
@@ -2,27 +2,69 @@ import { draftMode } from 'next/headers'
 import Image from 'next/image'
 
 import { getMoodboard } from '@/lib/api'
+import { altTextHelper } from '@/lib/helper-functions'
 
 const Page = async () => {
   const { isEnabled } = draftMode()
 
   const moodboard = await getMoodboard(isEnabled)
 
-  return (
-    <>
-      <h1 className="my-4 font-bold">moodboard</h1>
-      {moodboard.map((project) => {
-        return (
-          <div key={project.url}>
+  const renderGalleryRow = (imageGroup, index) => {
+    const imageOneURL = imageGroup[0].url
+    const imageOneTitle = imageGroup[0].title
+    let imageTwoURL
+    let imageTwoTitle
+
+    const twoImages = imageGroup.length === 2
+
+    if (twoImages) {
+      imageTwoURL = imageGroup[1].url
+      imageTwoTitle = imageGroup[1].title
+    }
+
+    return (
+      <div className="grid w-full grid-cols-1 lg:grid-cols-2" key={index}>
+        <div className="flex w-full items-end">
+          <Image
+            src={imageOneURL}
+            alt={altTextHelper(imageOneTitle)}
+            width={500}
+            height={500}
+            className="w-full"
+          />
+        </div>
+
+        {twoImages ? (
+          <div className="flex w-full items-end">
             <Image
-              src={project.url}
-              alt={project.title}
-              width="500"
-              height="500"
+              src={imageTwoURL}
+              alt={altTextHelper(imageTwoTitle)}
+              width={500}
+              height={500}
+              className="w-full"
             />
           </div>
-        )
-      })}
+        ) : null}
+      </div>
+    )
+  }
+
+  const imageMatrix = moodboard.reduce((rows, image, index) => {
+    return (
+      (index % 2 === 0
+        ? rows.push([image])
+        : rows[rows.length - 1].push(image)) && rows
+    )
+  }, [])
+
+  return (
+    <>
+      <h1 className="visually-hidden">moodboard</h1>
+      <div className="relative grid grid-cols-1 ">
+        {imageMatrix.map((imageGroup, index) => {
+          return renderGalleryRow(imageGroup, index)
+        })}
+      </div>
     </>
   )
 }
