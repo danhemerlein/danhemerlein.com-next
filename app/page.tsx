6553cb393c70bdd5bc339e41a87b944ab16e4b67
Dan Hemerlein <danhemerlein@gmail.com>
2024-12-12 23:06:03 -0500

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,7 @@
-import {
-  getAboutPage,
-  getAllCodeProjects,
-  getAllMusicProjects,
-} from '@/lib/api'
-
-import Code from './components/Code'
-import Hero from './components/Hero'
-import Music from './components/Music'
-
 const Home = async () => {
-  const allCodeProjects = await getAllCodeProjects()
-  const allMusicProjects = await getAllMusicProjects()
-
-  const aboutPage = await getAboutPage()
-
   return (
     <>
-      <Hero aboutPage={aboutPage} />
-      <Code allCodeProjects={allCodeProjects} />
-      <Music allMusicProjects={allMusicProjects} />
+      <h1>dan hemerlein</h1>
     </>
   )
 }
